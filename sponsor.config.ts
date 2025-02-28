b36f7c3437513d7553511f07929d0866d8c69ef7
Laugh <laugh12321@vip.qq.com>
2025-02-28 09:38:49 +0800

    Update sponsor.config.ts

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -1,4 +1,15 @@
-import { defineConfig, tierPresets } from 'sponsorkit'
+import { BadgePreset, defineConfig, tierPresets } from 'sponsorkit'
+
+const past: BadgePreset = {
+  avatar: {
+    size: 20,
+  },
+  boxWidth: 22,
+  boxHeight: 22,
+  container: {
+    sidePadding: 35,
+  },
+}
 
 export default defineConfig({
   tiers: [
@@ -8,33 +19,51 @@ export default defineConfig({
       preset: past,
     },
     {
-      title: '晨光赞助人',
+      title: 'Backers',
+      preset: tierPresets.small,
+    },
+    {
+      title: 'Sponsors',
       monthlyDollars: 10,
+      preset: {
+        avatar: {
+          size: 42,
+        },
+        boxWidth: 52,
+        boxHeight: 52,
+        container: {
+          sidePadding: 30,
+        },
+      }
+    },
+    {
+      title: '晨光赞助人',
+      monthlyDollars: 5,
       preset: tierPresets.xs,
     },
     {
       title: '造型赞助人',
-      monthlyDollars: 12,
+      monthlyDollars: 15,
       preset: tierPresets.small,
     },
     {
       title: '形象赞助人',
-      monthlyDollars: 13,
+      monthlyDollars: 30,
       preset: tierPresets.base,
     },
     {
       title: '明眸赞助人',
-      monthlyDollars: 14,
+      monthlyDollars: 50,
       preset: tierPresets.medium,
     },
     {
       title: '元气赞助人',
-      monthlyDollars: 15,
+      monthlyDollars: 80,
       preset: tierPresets.large,
     },
     {
       title: '光影赞助人',
-      monthlyDollars: 30,
+      monthlyDollars: 100,
       preset: tierPresets.xl,
     },
   ]
